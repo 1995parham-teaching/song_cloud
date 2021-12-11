cadd957b1f51e7e23a460ac7ef1b1a8c59403f43
Elahe Dastan <elahe.dstn@gmail.com>
2021-12-12 02:35:15 +0330

    fix: correct response package name

--- response/signup.go
+++ response/signup.go
@@ -5,11 +5,11 @@ import (
 )
 
 type User struct {
-	Username       string     `json:"username"`
-	Password       string     `json:"password"`
-	FirstName      string     `json:"first_name"`
-	LastName       string     `json:"last_name"`
-	Email          string     `json:"email"`
-	Score          int        `json:"score"`
-	SpecialTill    *time.Time `json:"special_till"`
+	Username    string     `json:"username"`
+	Password    string     `json:"password"`
+	FirstName   string     `json:"first_name"`
+	LastName    string     `json:"last_name"`
+	Email       string     `json:"email"`
+	Score       int        `json:"score"`
+	SpecialTill *time.Time `json:"special_till"`
 }

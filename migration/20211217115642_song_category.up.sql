c04df99fb0afd4c65399442df184a9227e16ddfc
mohamad <mh.setak38@gmail.com>
2021-12-17 19:18:38 +0330

    func getSongCategory added

--- migration/20211217115642_song_category.up.sql
+++ migration/20211217115642_song_category.up.sql
@@ -6,3 +6,21 @@ CREATE TABLE IF NOT EXISTS song_category
     CONSTRAINT FK_song FOREIGN KEY (song_id) REFERENCES song(id),
     CONSTRAINT FK_category FOREIGN KEY (category_id) REFERENCES category(id)
 );
+
+
+CREATE OR REPLACE FUNCTION get_song_category(song_id_in integer) 
+RETURNS TABLE(
+    category_id INT,
+    category_name varchar(255)
+)
+AS
+$$
+  BEGIN
+    RETURN QUERY
+        (SELECT b.id category_id, b.category_name 
+        FROM song_category a, category b
+        WHERE song_id_in = a.song_id
+            AND a.category_id = b.id);
+  END;
+$$
+LANGUAGE plpgsql;

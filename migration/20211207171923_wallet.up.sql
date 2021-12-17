73cfbab54e0e1328d64b39a2b3e4e85800ab3202
mohamad <mh.setak38@gmail.com>
2021-12-17 19:19:27 +0330

    charge credit func and purchase log trigger added

--- migration/20211207171923_wallet.up.sql
+++ migration/20211207171923_wallet.up.sql
@@ -15,12 +15,28 @@ BEGIN
     IF song_price = 0 THEN
       RETURN FALSE;
     END IF;
-    IF (NOT isPaid(username_in, song_id_in)) AND EXISTS (SELECT FROM wallet WHERE wallet.username = username_in and wallet.credit >= song_price) THEN
-      UPDATE wallet SET credit = credit - song_price WHERE wallet.username = username_in;
-      INSERT INTO purchase(username, song_id, paid) VALUES (username_in, song_id_in, song_price);
-      RETURN TRUE;
+    IF (NOT isPaid(username_in, song_id_in)) AND 
+      (NOT premium_user_validation(username_in)) AND
+      (NOT free(song_id_in)) AND
+      EXISTS (SELECT FROM wallet WHERE wallet.username = username_in and wallet.credit >= song_price) THEN
+        UPDATE wallet SET credit = credit - song_price WHERE wallet.username = username_in;
+        INSERT INTO purchase(username, song_id, paid) VALUES (username_in, song_id_in, song_price);
+        RETURN TRUE;
     END IF;
     RETURN FALSE;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION charge_credit(username_in varchar(255), amount_in INTEGER) RETURNS boolean as
+$$
+BEGIN
+    IF EXISTS(SELECT FROM users WHERE users.username = username_in) THEN
+      UPDATE wallet SET credit = credit + amount_in WHERE wallet.username = username_in;
+      RETURN true;
+    END IF;
+    RETURN false;
 END;
 $$
 LANGUAGE plpgsql;
--- migration/20211214114904_purchase.up.sql
+++ migration/20211214114904_purchase.up.sql
@@ -17,3 +17,16 @@ $$
 END;
 $$
 LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION purchase_log() RETURNS trigger as
+$$
+  BEGIN
+    INSERT INTO "log"(status_code, log_message, "time") 
+        VALUES (0, CONCAT(new.username, ' purchased this song: ', new.song_id), now());
+    RETURN NEW;
+  END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER tr_purchase_log AFTER INSERT on "purchase" FOR EACH ROW EXECUTE PROCEDURE purchase_log();

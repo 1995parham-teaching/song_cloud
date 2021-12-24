6afdaa43606be5168d9bfb4481b2e48b35515cc0
Elahe Dastan <elahe.dstn@gmail.com>
2021-12-24 12:57:40 +0330

    feat: add log user

--- migration/20211217150513_log.up.sql
+++ migration/20211217150513_log.up.sql
@@ -1,7 +1,10 @@
 CREATE TABLE IF NOT EXISTS log
 (
-    id              SERIAL primary key,
-    status_code INTEGER,
-    log_message varchar(500),
-    "time"  timestamp
+  id              SERIAL primary key,
+  status_code INTEGER,
+  log_message varchar(500),
+  "time"  timestamp
 );
+
+create role logman login password 'logman';
+grant select on log to logman;

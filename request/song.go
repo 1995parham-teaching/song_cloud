0636b0c1d9ca4cd6e056a7665d4412c64121e810
Elahe Dastan <elahe.dstn@gmail.com>
2021-12-24 09:55:37 +0330

    feat: add song request

---
+++ request/song.go
@@ -0,0 +1,14 @@
+package request
+
+type NewSong struct {
+	Name           string `json:"name"`
+	File           string `json:"file"`
+	ProductionYear int    `json:"production_year"`
+	Explanation    string `json:"explanation"`
+	Price          int    `json:"price"`
+}
+
+type PlaySong struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}

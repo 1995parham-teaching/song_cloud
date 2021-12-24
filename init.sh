60a83b90f7ea63d8c53b97f74eca9cd21da3d964
Elahe Dastan <elahe.dstn@gmail.com>
2021-12-24 12:58:49 +0330

    feat: add introducer for user creation

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 echo "creating users..."
 curl -d '{ "username": "elahe", "first_name": "Elahe", "last_name": "Dastan", "email": "elahe.dstn@gmail.com", "password": "123456abc" }' -H 'Content-Type: application/json' 127.0.0.1:8080/api/signup
-curl -d '{ "username": "foo", "first_name": "John", "last_name": "Doe", "email": "john.doe@gmail.com", "password": "123456abc" }' -H 'Content-Type: application/json' 127.0.0.1:8080/api/signup
+curl -d '{ "username": "foo", "first_name": "John", "last_name": "Doe", "email": "john.doe@gmail.com", "password": "123456abc", "introducer": "elahe" }' -H 'Content-Type: application/json' 127.0.0.1:8080/api/signup
 
 echo "creating songs..."
 curl -d '{ "new": "elahe", "file": "elahe.mp3", "production_year": 2021, "explanation": "new awesome song" }' -H 'Content-Type: application/json' 127.0.0.1:8080/api/song

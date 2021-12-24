7f6f79da9ff2e183331d35cd9cbde692648a5771
Elahe Dastan <elahe.dstn@gmail.com>
2021-12-24 12:58:35 +0330

    feat: add introducer

--- request/signup.go
+++ request/signup.go
@@ -1,9 +1,10 @@
 package request
 
 type Signup struct {
-	Username       string `json:"username"`
-	Password       string `json:"password"`
-	FirstName      string `json:"first_name"`
-	LastName       string `json:"last_name"`
-	Email          string `json:"email"`
+	Username   string  `json:"username"`
+	Password   string  `json:"password"`
+	FirstName  string  `json:"first_name"`
+	LastName   string  `json:"last_name"`
+	Email      string  `json:"email"`
+	Introducer *string `json:"introducer"`
 }
